6fdfc4a3f01e4b773c81d618715fb3ea81cba26c
Bencsik Gergő <bencsik.gergo@rufftech.hu>
2017-01-31 15:34:02 +0100

    use $stateParams

--- app/pages/product/product.js
+++ app/pages/product/product.js
@@ -2,19 +2,19 @@
     'use strict';
 
     angular.module('productListDemo')
-        .controller('begProductController', ['$state', 'repository', begProductController])
+        .controller('begProductController', ['$stateParams', 'repository', begProductController])
         .component('product', {
             templateUrl: './app/pages/product/product.html',
             controller: 'begProductController',
             controllerAs: 'vm'
         });
 
-    function begProductController($state, repository) {
+    function begProductController($stateParams, repository) {
         /* jshint validthis: true */
         var vm = this;
 
         vm.$onInit = function () {
-            repository.getProduct($state.params.id).then(function (response) {
+            repository.getProduct($stateParams.id).then(function (response) {
                 vm.product = response.data;
             });
         };
